88b3d1a299cdab3e8982e3f35c84c96f60824460
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2019-10-26 21:43:06 +0900

    Add UnixtimeStringField to parse unixtime as string (#4)
    * Add UnixtimeStringField to parse unixtime as string
    
    * Replace if-else to switch style of JSON.Parse

--- parser/generic.go
+++ parser/generic.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/m-mizutani/rlogs"
@@ -11,11 +12,12 @@ import (
 
 // JSON is basic json log parser.
 type JSON struct {
-	Tag                string
-	UnixtimeField      *string
-	UnixtimeMilliField *string
-	TimestampField     *string
-	TimestampFormat    *string
+	Tag                 string
+	UnixtimeField       *string
+	UnixtimeStringField *string
+	UnixtimeMilliField  *string
+	TimestampField      *string
+	TimestampFormat     *string
 }
 
 // Parse of JSON parses a json formatted log message.
@@ -28,32 +30,46 @@ func (x *JSON) Parse(msg *rlogs.MessageQueue) ([]*rlogs.LogRecord, error) {
 	}
 
 	var t time.Time
-	if field := x.UnixtimeField; field != nil {
-		if ts, ok := value[*field].(float64); ok {
+	switch {
+	case x.UnixtimeField != nil:
+		if ts, ok := value[*x.UnixtimeField].(float64); ok {
 			t = time.Unix(int64(ts), 0).UTC()
 		} else {
-			return nil, fmt.Errorf("No unixtime field (%s): %v", *field, value)
+			return nil, fmt.Errorf("No unixtime field (%s): %v", *x.UnixtimeField, value)
 		}
-	} else if field := x.UnixtimeMilliField; field != nil {
-		if ts, ok := value[*field].(float64); ok {
+
+	case x.UnixtimeMilliField != nil:
+		if ts, ok := value[*x.UnixtimeMilliField].(float64); ok {
 			t = time.Unix(int64(ts)/1000, (int64(ts)%1000)*1000).UTC()
 		} else {
-			return nil, fmt.Errorf("No unixtime milliseconds field (%s): %v", *field, value)
+			return nil, fmt.Errorf("No unixtime milliseconds field (%s): %v", *x.UnixtimeMilliField, value)
 		}
-	} else if field := x.TimestampField; field != nil {
+
+	case x.UnixtimeStringField != nil:
+		if str, ok := value[*x.UnixtimeStringField].(string); ok {
+			ts, err := strconv.ParseInt(str, 10, 64)
+			if err != nil {
+				return nil, errors.Wrapf(err, "Fail to parse UnixTimeString: %v", str)
+			}
+			t = time.Unix(int64(ts), 0).UTC()
+		} else {
+			return nil, fmt.Errorf("No unixtime milliseconds field (%s): %v", *x.UnixtimeStringField, value)
+		}
+
+	case x.TimestampField != nil:
 		if x.TimestampFormat == nil {
 			return nil, fmt.Errorf("TimestampFormat is required, but not set")
 		}
 
-		if ts, ok := value[*field].(string); ok {
+		if ts, ok := value[*x.TimestampField].(string); ok {
 			if p, err := time.Parse(*x.TimestampFormat, ts); err == nil {
 				t = p.UTC()
 			} else {
 				return nil, errors.Wrapf(err, "Fail to parse timestamp field by format '%s': %v", *x.TimestampFormat, value)
 			}
-
 		}
-	} else {
+
+	default:
 		return nil, fmt.Errorf("No timestamp field arguments. One of UnixtimeField, UnixtimeMilliField and TimestampField is required")
 	}
 
