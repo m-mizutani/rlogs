19cfc5762a1b0980329a6d7681471569d9a853bc
Masayoshi Mizutani <mizutani@cookpad.com>
2020-08-15 15:58:16 +0900

    Export NewS3Client

--- s3.go
+++ s3.go
@@ -10,7 +10,8 @@ type s3Client interface {
 	GetObject(input *s3.GetObjectInput) (*s3.GetObjectOutput, error)
 }
 
-var newS3Client = newAwsS3Client
+// NewS3Client is constructor of AWS S3 client. It can be replaced for testing
+var NewS3Client = newAwsS3Client
 
 func newAwsS3Client(region string) s3Client {
 	ssn := session.Must(session.NewSession(&aws.Config{
